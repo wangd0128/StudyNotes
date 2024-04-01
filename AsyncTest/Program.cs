66a9655bf68b27efae6c33e22de2b093bd94ba40
冬 王 <wangd910128@163.com>
2024-04-01 14:30:51 +0800

    解决方案创建，异步项目测试

---
+++ AsyncTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    internal static void Main(string[] args)
+    {
+        DoSth();
+        Console.ReadLine();
+    }
+
+    internal static async void DoSth()
+    {
+        Console.WriteLine("1");
+        await "1";
+        Test test = new Test();
+        Console.WriteLine("2");
+        await test;
+        Console.WriteLine("3");
+    }
+}
+
+public class TestAwaiter : INotifyCompletion
+{
+    public bool IsCompleted { get; set; }
+
+    public void OnCompleted(Action continuation)
+    {
+        Console.WriteLine("OnCompleted");
+        continuation?.Invoke();
+    }
+
+    public void GetResult()
+    {
+        Console.WriteLine("GetResult");
+    }
+}
+
+public class Test
+{
+    public TestAwaiter GetAwaiter()
+    {
+        return new TestAwaiter();
+        //return Task.FromResult(0);
+    }
+}
+
+public static class StringExtensions
+{
+    public static TestAwaiter GetAwaiter(this string str)
+    {
+        return new TestAwaiter();
+        //return Task.FromResult(0);
+    }
+}
