91be460e3939ad56823a440e6de977fd678c665a
冬 王 <wangd910128@163.com>
2024-04-03 17:43:59 +0800

    update C#内存测试 同步块索引

---
+++ TestCapacity/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[StructLayout(LayoutKind.Sequential)]
+public class Test
+{
+    public int MyProperty { get; set; }
+
+    public long MyProperty1 { get; set; }
+
+    public int MyProperty2 { get; set; }
+
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int size = GetBytes(() =>
+        {
+            var test = new Test();
+        });
+        var size1 = Marshal.SizeOf(typeof(Test));
+        var size2 = Unsafe.SizeOf<Test>();
+        Console.WriteLine($"GetBytes {size}");
+        Console.WriteLine($"Marshal.SizeOf {size1}");
+        Console.WriteLine($"Unsafe.SizeOf {size2}");
+        Console.ReadLine();
+    }
+
+    private static int GetBytes(Action action)
+    {
+        GC.Collect();
+        GC.WaitForFullGCComplete();
+        var start = GC.GetAllocatedBytesForCurrentThread();
+        action?.Invoke();
+        GC.Collect();
+        GC.WaitForFullGCComplete();
+        var end = GC.GetAllocatedBytesForCurrentThread();
+        return (int)(end - start);
+    }
+}
